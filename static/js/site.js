fc82d0bfa9988dcf77aba4e56b83cb9fc1c73c6a
Andy McKay <amckay@mozilla.com>
2012-09-14 09:26:45 -0700

    js?

--- static/js/site.js
+++ static/js/site.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('#validate').bind('submit', function() {
         $.post($('#validate').attr('action'), $('#validate').serialize(),
                 function(data) {
-                    $('section.output').append($('p', {'text': data}));
+                    $('section.output').append('<p>' + data + '</p>');
                 });
         return false;
     });
